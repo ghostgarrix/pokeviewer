bc4a6fae68442133b7ba816a47b8eeceb9449aff
nazimk <nazim@lovebox.love>
2022-09-09 14:11:12 +0200

    upgraded expo and updated readme

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,34 +6,37 @@ import { AppStackParamList, AppStackRoutes } from "./routes";
 import { registerRootComponent } from "expo";
 import { HomePage } from "@src/pages/Home";
 import { DetailsPage } from "@src/pages/Details";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const Stack = createStackNavigator<AppStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName={AppStackRoutes.Home}
-        screenOptions={{
-          cardStyle: { backgroundColor: "#fff" },
-        }}
-      >
-        <Stack.Screen
-          name={AppStackRoutes.Home}
-          component={HomePage}
-          options={{ title: "Pokemons" }}
-        />
-        <Stack.Screen
-          name={AppStackRoutes.Details}
-          component={DetailsPage}
-          options={({ route }) => ({
-            title:
-              route.params.pokemon.name.charAt(0).toUpperCase() +
-              route.params.pokemon.name.slice(1),
-          })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName={AppStackRoutes.Home}
+          screenOptions={{
+            cardStyle: { backgroundColor: "#fff" },
+          }}
+        >
+          <Stack.Screen
+            name={AppStackRoutes.Home}
+            component={HomePage}
+            options={{ title: "Pokemons" }}
+          />
+          <Stack.Screen
+            name={AppStackRoutes.Details}
+            component={DetailsPage}
+            options={({ route }) => ({
+              title:
+                route.params.pokemon.name.charAt(0).toUpperCase() +
+                route.params.pokemon.name.slice(1),
+            })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
