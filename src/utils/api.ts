550d2b380391ade331d3a14a707d6e91b8eedbbc
nazimk <nazim@lovebox.love>
2022-09-15 18:35:55 +0200

    deleted comments

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -12,10 +12,6 @@ export const fetchAllPokemons = async (): Promise<Pokemon[]> => {
   } catch (error) {
     throw new Error("reading data went wrong");
   }
-  // fetch(`https://pokeapi.co/api/v2/pokemon?limit=${FETCH_LIMIT}`)
-  //   .then((response) => response.json())
-  //   .then((reponse) => setPokemons(reponse.results))
-  //   .catch((error) => console.log(error));
 };
 
 export const fetchPokemonsByType = async (
@@ -38,24 +34,6 @@ export const fetchPokemonsByType = async (
       throw new Error("reading data went wrong");
     }
   }
-
-  // if (type === "all" || type === null) {
-  //   fetchAllPokemons();
-  // } else {
-  //   fetch(`https://pokeapi.co/api/v2/type/${type}`)
-  //     .then((response) => response.json())
-  //     .then((response) => {
-  //       setPokemons(
-  //         response.pokemon.map((value: any) => {
-  //           return {
-  //             name: value.pokemon.name,
-  //             url: value.pokemon.url,
-  //           };
-  //         })
-  //       );
-  //     })
-  //     .catch((error) => console.log(error));
-  // }
 };
 
 export const fetchTypes = async (): Promise<TypeFilter[]> => {
@@ -77,22 +55,6 @@ export const fetchTypes = async (): Promise<TypeFilter[]> => {
   } catch (error) {
     throw new Error("reading data went wrong");
   }
-  // fetch(`https://pokeapi.co/api/v2/type`)
-  //   .then((response) => response.json())
-  //   .then((response): void => {
-  //     const tmpTypes: TypeFilter[] = response.results.map((value: Type) => {
-  //       return {
-  //         value: value.name,
-  //         label: value.name.charAt(0).toUpperCase() + value.name.slice(1),
-  //       };
-  //     });
-  //     tmpTypes.unshift({
-  //       value: "all",
-  //       label: "All",
-  //     });
-  //     setItems(tmpTypes);
-  //   })
-  //   .catch((error) => console.log(error));
 };
 
 export const fetchPokemonDetails = async (
@@ -105,10 +67,6 @@ export const fetchPokemonDetails = async (
   } catch (error) {
     throw new Error("reading data went wrong");
   }
-  // fetch(`${BASE_URL}pokemon/${pokeName}`)
-  //   .then((res) => res.json())
-  //   .then((details) => setPokeInfo(details))
-  //   .catch((error) => console.log(error));
 };
 
 export const getLink = (pokeName: string): string =>
